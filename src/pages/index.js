d66f1109e0d8badc89925d00caa6352537cb1afb
Nore5515 <Nore5515@gmail.com>
2020-02-03 14:39:14 -0600

    RBX implementation, initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "rbx/index.css";
-import { Content } from "rbx";
+import { Content, Title, Button} from "rbx";
+import Layout from "../components/layout"
 
 class App extends React.Component {
   constructor(props){
@@ -32,7 +33,7 @@ class App extends React.Component {
     if (this.score >= this.howdyPrice){
       this.score = this.score - this.howdyPrice;
       this.howdyCount = this.howdyCount + 1;
-      this.howdyPrice = this.howdyPrice + (7 * this.howdyCount);
+      this.howdyPrice = this.howdyPrice + (2 * this.howdyCount);
       this.setState((state, props) => {
         this.state.note.push(<div><br/>Howdy! +1/sec</div>);
         return {
@@ -41,6 +42,7 @@ class App extends React.Component {
         };
       });
     }
+    this.setState({});
   }
 
   upgradeHowdy = () => {
@@ -49,7 +51,7 @@ class App extends React.Component {
       this.howdyGain = this.howdyGain * 2;
       this.upgradeHowdy1 = true;
     }
-
+    this.setState({});
   }
 
   sayHowdy = () => {
@@ -73,13 +75,24 @@ class App extends React.Component {
 
   getButtons = () => {
     return (
-      <div>
-      <button onClick={this.sayHowdy}>Say Howdy!</button><br/>
-      <button onClick={this.Howdy}>Make a Howdy ({this.howdyPrice})</button>
-      <button onClick={this.getCowboy}>Hire a Cowboy ({this.cowboyPrice})</button><br/>
-      <button disabled={this.upgradeHowdy1}onClick={this.upgradeHowdy}>Emphasize the "dee" in Howdy: Double Howdy gain. (200)</button>}
-      </div>
+      <>
+      <Button rounded size={"large"} onClick={this.sayHowdy}>Say Howdy!</Button><br/>
+      <Button onClick={this.Howdy}>Make a Howdy ({this.howdyPrice})</Button>
+      <Button onClick={this.getCowboy}>Hire a Cowboy ({this.cowboyPrice})</Button><br/>
+      <Button disabled={this.upgradeHowdy1}onClick={this.upgradeHowdy}>Emphasize the "dee" in Howdy: Double Howdy gain. (200)</Button>
+      </>
     )
+  }
+
+  getGain = () => {
+    if (this.gain == 0){return (<gain0>{this.gain}</gain0>)}
+    else if (this.gain < 5 && this.gain > 0){return (<gain1>{this.gain}</gain1>)}
+    else if (this.gain < 20 && this.gain > 5){return (<gain2>{this.gain}</gain2>)}
+    else if (this.gain < 50 && this.gain > 20){return (<gain3>{this.gain}</gain3>)}
+    else if (this.gain < 100 && this.gain > 50){return (<gain4>{this.gain}</gain4>)}
+    else if (this.gain < 300 && this.gain > 100){return (<gain5>{this.gain}</gain5>)}
+    else if (this.gain < 1000 && this.gain > 300){return (<gain6>{this.gain}</gain6>)}
+    else {return (<gain7>{this.gain}</gain7>)}
   }
 
   componentDidMount() {
@@ -102,11 +115,11 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <h1>{this.score}</h1><br/>
-        Total How's Deed/second: {this.gain}
-        <p>{this.howdyCount + " Howdy's"}</p>
+        <startValue>{this.score}</startValue><br/>
+        Total How's Deed/second: {this.getGain()} <br/>
+        {this.howdyCount + " Howdy's"}<br/>
         {this.cowboyCount + " Cowboy's"}<br/>
-
+        {this.getButtons()}
         {this.state.note.map(
           (item, index) => (this.state.note[index])
         )}
@@ -114,10 +127,10 @@ class App extends React.Component {
     )
   }
 }
-
+//<Title>E</Title>
 //export default App
 
-export default () => <App />
+export default () => <Layout><App /></Layout>
 
 
 
